0935b339e7faf21f70d81fa13baccd8d83d051db
Derek <derek.r.roy@gmail.com>
2020-03-12 17:07:56 -0700

    Transform functions
    added relu activation function and soft max transformation to separate file

---
+++ transforms.py
@@ -0,0 +1,7 @@
+# Activation (Rectified Linear Unit) and modifies so if value < 0: value = 0   
+    def relu(self, x):
+        return np.maximum(0,x)
+    
+    # Soft max to transform last fully connected layer in network 
+    def softmax(logits):
+        return np.exp(logits)/np.sum(np.exp(logits), axis=0)
