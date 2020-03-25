d9f35f265bd1062da1cfb33830b03dcd897a0c3e
Derek <derek.r.roy@gmail.com>
2020-03-25 07:40:42 -0700

    Added partial derivative functions
    Added partial derivative functions for all activation functions. 
    Removed Soft max activation as it was not necessary for this project .

--- activations.py
+++ activations.py
@@ -4,14 +4,26 @@ import numpy as np
 def relu(x):
     return np.maximum(0,x)
 
+# Derivative of relu in regards to output
+def relu_backprop(out):
+    out[out > 0] = 1
+    out[out < 0] = 0
+    return out
+
 # Activation (Leaky Rectified Linear Unit): if value < .01*value: value = .01*value   
 def leaky_relu(x):
     return np.maximum(.01*x,x)
+
+# Derivative of leaky relu in regards to output
+def leaky_relu_backprop(out):
+    out[out > 0] = 1
+    out[out < 0] = .01
+    return out
 
 # Sigmoid activation
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
-# Soft max to transform last fully connected layer in network 
-def softmax(logits):
-    return np.exp(logits)/np.sum(np.exp(logits), axis=0)
+# Derivative of sigmoid in regards to output
+def sigmoid_backprop(out):
+    return out*(1-out)
