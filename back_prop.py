53c914167f96ee9304543f7c8806cd05a73220ba
Derek <derek.r.roy@gmail.com>
2020-03-25 06:06:51 -0700

    added numpy import and functions
    added the import of numpy as np
    added sum squared error function added the root mean squared error function and comments 
    changed file name to be error_functions

--- back_prop.py
+++
@@ -1,3 +0,0 @@
-# Cross entropy error function 
-def cross_entropy(labels,predictions):
-    return np.sum(labels*np.log2(predictions))*-1
---
+++ error_functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Cross entropy error function 
+def cross_entropy(t,y):
+    return np.sum(t*np.log2(y))*-1
+
+# Root mean square error function 
+def rmse(t,y)
+    return np.sqrt(sum_sqaured_error(y,t)/len(t))
+
+# Sum squared error function
+def sum_sqaured_error(t,y):
+    return np.sum(np.power((t-y),2))
