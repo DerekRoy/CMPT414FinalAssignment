876820750d20424708dc8eb5579df5c2153b0384
Derek <derek.r.roy@gmail.com>
2020-03-24 18:10:47 -0700

    Added Functions
    Added sigmoid and leaky relu

--- activations.py
+++ activations.py
@@ -4,6 +4,14 @@ import numpy as np
 def relu(x):
     return np.maximum(0,x)
 
+# Activation (Leaky Rectified Linear Unit): if value < .01*value: value = .01*value   
+def leaky_relu(x):
+    return np.maximum(.01*x,x)
+
+# Sigmoid activation
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
 # Soft max to transform last fully connected layer in network 
 def softmax(logits):
     return np.exp(logits)/np.sum(np.exp(logits), axis=0)
