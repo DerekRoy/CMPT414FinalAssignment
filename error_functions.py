0df0ec8f8b97bea056db6beb44d9fa326bb11c01
Derek <derek.r.roy@gmail.com>
2020-03-31 07:07:20 -0700

    Added derivative function
    added a derivative function for cross entropy

--- error_functions.py
+++ error_functions.py
@@ -4,6 +4,10 @@ import numpy as np
 def cross_entropy(t,y):
     return np.sum(t*np.log2(y))*-1
 
+# Derivative of Cross entropy for backprop
+def backprop_cross_entropy(t,y):
+    return -t/y
+
 # Root mean square error function 
 def rmse(t,y)
     return np.sqrt(sum_sqaured_error(y,t)/len(t))
