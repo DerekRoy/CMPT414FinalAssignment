a5aa314af200c2c3ca7071e1e6a81990cc3467ee
Derek <derek.r.roy@gmail.com>
2020-04-09 14:46:53 -0700

    Added training and testing function
    training and testing function to build and evaluate model

--- cnn.py
+++ cnn.py
@@ -51,6 +51,109 @@ class CNN:
     def predict(self,image):
         return self.feed_forward(image)
     
+    # Train the model with training data
+    def train(self,X_train,y_train,epochs=10):
+        # Run through each epoch
+        for e in range(epochs):
+            print("Epoch {}/{}".format(e+1,epochs))
+
+            # Random Shuffle on data and split to create train and validation set
+            X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
+
+            # Place holders for loss, accuracy, and validation loss and accuracy per epoch 
+            loss = []
+            accuracy = []
+            val_loss = []
+            val_accuracy = []
+            
+            # Metrics to update progress of model
+            num_images = X_train.shape[0]
+            last_percentage = 0
+            print("\tPercent finished in training {}%".format(last_percentage), end = ' ')
+            
+            # Train the model 
+            for i,image in enumerate(X_train):
+                print("\t {}/{}".format(i,num_images))
+                # Get percent done with epoch
+#                 percent_done = int(i/num_images*100)
+#                 if percent_done%10 == 0 and last_percentage != percent_done:
+#                     print("{}%".format(percent_done), end = ' ')
+#                     last_percentage = percent_done
+                
+                # Predict number 
+                prediction = self.feed_forward(image)
+
+                # Add loss to epoch loss
+                loss.append(cross_entropy(y_train[i],prediction))
+
+                # Get number of correct predictions and wrong predictions
+                if np.argmax(y_train[i]) == np.argmax(prediction):
+                    accuracy.append(1)
+                else: 
+                    accuracy.append(0)
+
+                # Do Back Propogation
+            #     self.backprop(y_train[i],prediction)
+            
+            last_percentage = 0
+            print("\n\tPercent finished in validation {}%".format(last_percentage), end = ' ')
+            
+            # Get validation
+            for i,image in enumerate(X_val):
+                print("\t {}/{}".format(i,num_images))
+#                 # Get percent done with epoch
+#                 percent_done = int(i/num_images*100)
+#                 if percent_done%10 == 0 and last_percentage != percent_done:
+#                     print("{}%".format(percent_done), end = ' ')
+#                     last_percentage = percent_done
+                
+                # Predict Number
+                prediction = self.feed_forward(image)
+
+                # Add loss to val loss
+                val_loss.append(cross_entropy(y_val[i],prediction))
+
+                # Get number of correct predictions and wrong predictions
+                if np.argmax(y_val[i]) == np.argmax(prediction):
+                    val_accuracy.append(1)
+                else: 
+                    val_accuracy.append(0)
+
+            # Get averages over epochs
+            loss = np.mean(loss)
+            accuracy = np.mean(accuracy)
+            val_loss = np.mean(val_loss)
+            val_accuracy = np.mean(val_accuracy)
+
+            # Print out preformance
+            print("\ttrain loss: {} train accuracy: {} \n\tvalidation loss: {} validation accuracy: {}\n".format(loss,accuracy,val_loss,val_accuracy))
+    
+    # Test the accuracy of model with test set
+    def test(self,X_test,y_test):
+        # Loss and accuracy place holders
+        loss = []
+        accuracy = []
+        
+        # Go through test data and run predictions
+        for i,image in enumerate(X_test):
+            # Predict Number
+            prediction = self.feed_forward(image)
+            
+            # Add loss to epoch loss
+            loss.append(cross_entropy(y_test[i],prediction))
+            
+            # Get number of correct predictions and wrong predictions
+            if np.argmax(y_val[i]) == np.argmax(prediction):
+                accuracy.append(1)
+            else: 
+                accuracy.append(0)
+        
+        # Calculate average loss and accuracy
+        loss = np.mean(loss)
+        accuracy = np.mean(accuracy)
+        
+        print("Loss: {} Accuracy: {}".format(loss,accuracy))
+    
     # Save the model weights to a pickle file
     def save_model(self, name="weights"):
         # Put weights into dictionary to be saved
