afe253be444f97470fc25bc50616dba041b6646c
Derek <derek.r.roy@gmail.com>
2020-04-09 16:28:51 -0700

    Changes made to simplify cnn run fc
    simplified network architecture of cnn
    moved equation in fc
    got weights

--- cnn.py
+++ cnn.py
@@ -4,52 +4,36 @@
 #     d = discriminator(image)
 #     d.run(image)
 
-from conv import convolution_layer 
+from conv import convolution_layer
 from activations import leaky_relu,softmax,leaky_relu_backprop
 from flatten import flatten
 from fc import fully_connected_layer
-from error_functions import backprop_cross_entropy, cross_entropy
+from error_functions import backprop_cross_entropy
 import numpy as np
 import pickle
+from sklearn.model_selection import train_test_split
+from error_functions import cross_entropy
 
 class CNN:
     def __init__(self,image):
-        # Create place holder variables
-        self.image = []
-        self.c1_out = []
-        self.c2_out = []
-        self.output = []
-        
-        # Set initialized convolutional layers
         self.c1 = convolution_layer(image.shape,64,5,2,True)
-        self.c2 = convolution_layer(self.c1.out_dim,128,5,2,True)
-        self.flat = flatten(self.c2.out_dim)
+        self.flat = flatten(self.c1.out_dim)
         self.dense = fully_connected_layer(self.flat.output_shape[0],10)
-    
+
     def feed_forward(self,image):
-        # Image input
-        self.image = image
-        
         # Convolutional layer 1
         self.c1_out = self.c1.conv(image)
         self.c1_activation = leaky_relu(self.c1_out)
-        
-        # Convolutional layer 2
-        self.c2_out = self.c2.conv(self.c1_activation)
-        self.c2_activation = leaky_relu(self.c2_out)
-        
-        # Flatten out 
-        self.flattened = self.flat.flatten(self.c2_activation)
-        
-        # Out Layer 
-        self.output = self.dense.feed_forward(self.flattened)[0]
+
+        # Flatten out
+        self.flattened = self.flat.flatten(self.c1_activation)
+
+        # Out Layer
+        self.logits = self.dense.feed_forward(self.flattened)
+        self.output = softmax(self.logits)
 
         return self.output
-    
-    # Run prediction 
-    def predict(self,image):
-        return self.feed_forward(image)
-    
+
     # Train the model with training data
     def train(self,X_train,y_train,epochs=10):
         # Run through each epoch
@@ -59,18 +43,18 @@ class CNN:
             # Random Shuffle on data and split to create train and validation set
             X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
 
-            # Place holders for loss, accuracy, and validation loss and accuracy per epoch 
+            # Place holders for loss, accuracy, and validation loss and accuracy per epoch
             loss = []
             accuracy = []
             val_loss = []
             val_accuracy = []
-            
+
             # Metrics to update progress of model
             num_images = X_train.shape[0]
             last_percentage = 0
             print("\tPercent finished in training {}%".format(last_percentage), end = ' ')
-            
-            # Train the model 
+
+            # Train the model
             for i,image in enumerate(X_train):
                 print("\t {}/{}".format(i,num_images))
                 # Get percent done with epoch
@@ -78,8 +62,8 @@ class CNN:
 #                 if percent_done%10 == 0 and last_percentage != percent_done:
 #                     print("{}%".format(percent_done), end = ' ')
 #                     last_percentage = percent_done
-                
-                # Predict number 
+
+                # Predict number
                 prediction = self.feed_forward(image)
 
                 # Add loss to epoch loss
@@ -88,15 +72,15 @@ class CNN:
                 # Get number of correct predictions and wrong predictions
                 if np.argmax(y_train[i]) == np.argmax(prediction):
                     accuracy.append(1)
-                else: 
+                else:
                     accuracy.append(0)
 
                 # Do Back Propogation
-                self.back_prop(y_train[i])
-            
+            #     self.backprop(y_train[i],prediction)
+
             last_percentage = 0
             print("\n\tPercent finished in validation {}%".format(last_percentage), end = ' ')
-            
+
             # Get validation
             for i,image in enumerate(X_val):
                 print("\t {}/{}".format(i,num_images))
@@ -105,7 +89,7 @@ class CNN:
 #                 if percent_done%10 == 0 and last_percentage != percent_done:
 #                     print("{}%".format(percent_done), end = ' ')
 #                     last_percentage = percent_done
-                
+
                 # Predict Number
                 prediction = self.feed_forward(image)
 
@@ -115,7 +99,7 @@ class CNN:
                 # Get number of correct predictions and wrong predictions
                 if np.argmax(y_val[i]) == np.argmax(prediction):
                     val_accuracy.append(1)
-                else: 
+                else:
                     val_accuracy.append(0)
 
             # Get averages over epochs
@@ -126,52 +110,50 @@ class CNN:
 
             # Print out preformance
             print("\ttrain loss: {} train accuracy: {} \n\tvalidation loss: {} validation accuracy: {}\n".format(loss,accuracy,val_loss,val_accuracy))
-    
+
     # Test the accuracy of model with test set
     def test(self,X_test,y_test):
         # Loss and accuracy place holders
         loss = []
         accuracy = []
-        
+
+        # Images to test
+        images = X_test.shape[0]
+
         # Go through test data and run predictions
         for i,image in enumerate(X_test):
+            print("Testing: {}/{} images".format(i+1,images))
             # Predict Number
             prediction = self.feed_forward(image)
-            
+
             # Add loss to epoch loss
             loss.append(cross_entropy(y_test[i],prediction))
-            
+
             # Get number of correct predictions and wrong predictions
-            if np.argmax(y_val[i]) == np.argmax(prediction):
+            if np.argmax(y_test[i]) == np.argmax(prediction):
                 accuracy.append(1)
-            else: 
+            else:
                 accuracy.append(0)
-        
+
         # Calculate average loss and accuracy
         loss = np.mean(loss)
         accuracy = np.mean(accuracy)
-        
+
         print("Loss: {} Accuracy: {}".format(loss,accuracy))
-    
-    # Save the model weights to a pickle file
+
+
     def save_model(self, name="weights"):
-        # Put weights into dictionary to be saved
-        weights = {"c1_weights":self.c1.get_filters(),"c2_weights":self.c2.get_filters(),"dense_weights":self.dense.get_weights()}
-        
-        # Open file and save with pickle
+        weights = {"c1_weights":self.c1.get_filters(),"dense_weights":self.dense.get_weights(),"dense_bias":self.dense.get_bias()}
         with open(name, 'wb') as pickle_file:
             pickle.dump(weights, pickle_file)
         print("Model Saved")
-   
-    # Load a model from a pickle file
+
     def load_model(self,name="weights"):
         with open(name, 'rb') as f:
             weights = pickle.load(f)
-
-        # Set all the weights from the pickle file
         self.c1.set_filters(weights['c1_weights'])
-        self.c2.set_filters(weights['c2_weights'])
         self.dense.set_weights(weights['dense_weights'])
+        self.dense.set_bias(weights['dense_bias'])
         print("Model Loaded")
 
     def back_prop(self, desired_output):
--- fc.py
+++ fc.py
@@ -1,5 +1,5 @@
 import numpy as np
-from activations import sigmoid_backprop, sigmoid, softmax, softmax_backprop
+from activations import sigmoid_backprop, sigmoid
 
 R = 0.1
 
@@ -14,31 +14,40 @@ class fully_connected_layer:
         self.cache_output = None
 
         self.drop_out = dropout
-    
+
     # Return the weights matrix
     def get_weights(self):
         return self.weights
-    
+
     # Set the values for the weights matrix
     def set_weights(self,w):
         self.weights = w
-    
+
+    # Return the bias array
+    def get_bias(self):
+        return self.biases
+
+    # Set the values for the biases array
+    def set_bias(self,b):
+        self.biases = b
+
     # Return the output size
     def out(self):
         return self.nodes
 
     def feed_forward(self, x):
-        output = np.dot(self.weights, x) + self.biases
+        output = np.dot(x,self.weights) + self.biases
 
         self.cache_x = x
         self.cache_output = output
 
-        return softmax(output), output
+        return output
 
     def back_prop(self, desired_output, actual_output):
         desired_output = desired_output.reshape(actual_output.shape)
+        dX = -(np.divide(desired_output, actual_output) - np.divide(1 - desired_output, 1 - actual_output))
 
-        delta_output = actual_output - desired_output
+        delta_output = sigmoid_backprop(dX, self.cache_output)
         delta_weights = np.dot(delta_output, self.cache_x.T) / self.cache_x.shape[1]
         delta_biases = np.sum(delta_output, axis=1, keepdims=True) / self.cache_x.shape[1]
         grad = np.dot(delta_weights.T, delta_output)
