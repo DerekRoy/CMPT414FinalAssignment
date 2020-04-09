a15ad48598fef879d65cdb40df4b1afff00aa506
Derek <derek.r.roy@gmail.com>
2020-04-09 10:36:16 -0700

    save and load models, variable storage, comments
    added save and load functions for  model using pickle library
    turned all feed forward values into variables to use for back prop 
    added more comments and separation for easier reading

--- cnn.py
+++ cnn.py
@@ -5,21 +5,65 @@
 #     d.run(image)
 
 from conv import convolution_layer 
-from activations import leaky_relu,softmax
+from activations import leaky_relu,softmax,leaky_relu_backprop
 from flatten import flatten
 from fc import fully_connected_layer
+from error_functions import backprop_cross_entropy
+import numpy as np
+import pickle
 
-class discriminator:
+class CNN:
     def __init__(self,image):
-        c1 = convolution_layer(image.shape,64,5,2,True)
-        c2 = convolution_layer(c1.out_dim,128,5,2,True)
-        flat = flatten(c2.out_dim)
-        dense = fully_connected_layer(flat.output_shape[0],10)
+        # Create place holder variables
+        self.image = []
+        self.c1_out = []
+        self.c2_out = []
+        self.output = []
+        
+        # Set initialized convolutional layers
+        self.c1 = convolution_layer(image.shape,64,5,2,True)
+        self.c2 = convolution_layer(self.c1.out_dim,128,5,2,True)
+        self.flat = flatten(self.c2.out_dim)
+        self.dense = fully_connected_layer(self.flat.output_shape[0],10)
     
-    def run(self,image):
-        c1_out = c1.conv(image)
-        c1_relu = leaky_relu(c1_out)
-        c2_out = c2.conv(c1_relu)
-        c2_relu = leaky_relu(c2_out)
-        flattened = flat.flatten(c2_relu)
-        return softmax(dense.feed_forward(flattened))
+    def feed_forward(self,image):
+        # Image input
+        self.image = image
+        
+        # Convolutional layer 1
+        self.c1_out = self.c1.conv(image)
+        self.c1_activation = leaky_relu(self.c1_out)
+        
+        # Convolutional layer 2
+        self.c2_out = self.c2.conv(self.c1_activation)
+        self.c2_activation = leaky_relu(self.c2_out)
+        
+        # Flatten out 
+        self.flattened = self.flat.flatten(self.c2_activation)
+        
+        # Out Layer 
+        self.logits = self.dense.feed_forward(flattened)
+        self.output = softmax(self.logits)
+        
+        return self.output
+    
+    # Save the model weights to a pickle file
+    def save_model(self, name="weights"):
+        # Put weights into dictionary to be saved
+        weights = {"c1_weights":self.c1.get_filters(),"c2_weights":self.c2.get_filters(),"dense_weights":self.dense.get_weights()}
+        
+        # Open file and save with pickle
+        with open(name, 'wb') as pickle_file:
+            pickle.dump(weights, pickle_file)
+        print("Model Saved")
+   
+    # Load a model from a pickle file
+    def load_model(self,name="weights"):
+        with open(name, 'rb') as f:
+            weights = pickle.load(f)
+        
+        # Set all the weights from the pickle file
+        self.c1.set_filters(weights['c1_weights'])
+        self.c2.set_filters(weights['c2_weights'])
+        self.dense.set_weights(weights['dense_weights'])
+        print("Model Loaded")
